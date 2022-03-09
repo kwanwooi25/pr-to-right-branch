055310a5a3b4b6d37958d8cfd005f16f99635576
k.jeong <kwanwoo.jeong@gmail.com>
2022-03-09 21:37:00 +0900

    v0.1

---
+++ src/switchBranch.ts
@@ -0,0 +1,36 @@
+import { getInput } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+
+interface Args {
+  from: string;
+  to?: string;
+}
+
+export const switchBranch = async ({ from, to }: Args) => {
+  if (to) {
+    throw new Error('Need a correct base branch to switch');
+  }
+
+  const repoToken = getInput('repo-token', { required: true });
+  const octokit = getOctokit(repoToken);
+  const payload = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: Number(context.payload.pull_request?.number),
+  };
+
+  await octokit.rest.pulls.update({
+    ...payload,
+    base: to,
+  });
+
+  await octokit.rest.pulls.createReviewComment({
+    ...payload,
+    body:
+      '🚫 This PR was heading to wrong direction. Base branch switched from `' +
+      from +
+      '` to `' +
+      to +
+      '` automatically. 🤗',
+  });
+};
